03d09f3c3c62a4b3bb8ce8b75cea953412e309c8
Krys <kdchodak@gmail.com>
2019-11-21 22:27:02 +0100

    Add now/zeit and welcome message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
-
+      <h1 style={{ textAlign: 'center' }}>Welcome to Petster.</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
