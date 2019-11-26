d4dd82396834117270dd8d9aae66b4fa58483e13
Krys <kdchodak@gmail.com>
2019-11-27 00:08:21 +0100

    Add try catch WIP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import GlobalStyle from './styles/GlobalStyle'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Gallery from './Gallery.js'
 import AnimalProfile from './AnimalProfile'
 
 function App() {
-  const [animal, setAnimal] = useState(JSON.parse(localStorage.animal) || {})
+  const [animal, setAnimal] = useState({})
+  let localData = {}
+  try {
+     localData = JSON.parse(localStorage.animal)
+     console.log('tri', localData)
+  } catch (error) {
+    localData = {}
+    console.error('Fehler: ', error.message)
+  }
+  
   const database = require('./database.json')
   return (
     <Router>
@@ -15,7 +24,7 @@ function App() {
           <Gallery database={database} handleAnimal={handleAnimal} />
         </Route>
         <Route path="/animalprofile/*">
-          <AnimalProfile animal={animal} />
+          <AnimalProfile animal={!animal || localData} />
         </Route>
       </Switch>
 
@@ -25,8 +34,9 @@ function App() {
       </nav>
     </Router>
   )
+
   function handleAnimal(animal) {
-    setAnimal(animal === {} ? JSON.parse(localStorage.animal) : animal)
+    setAnimal(animal)
     localStorage.animal = JSON.stringify(animal)
   }
 }
